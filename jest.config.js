43571aa32d1072854995f03d5e88bb79598c16b7
mrlew <luis.reischak@gmail.com>
2020-07-17 21:52:56 -0300

    added babel-jest to transpile scss files

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,6 @@ module.exports = {
     '^components(.*)$': '<rootDir>/client/js/components$1',
     '^store(.*)$': '<rootDir>/client/js/store$1',
     '^app/utils(.*)$': '<rootDir>/client/js/utils$1',
+    '^.+\\.(css|less|scss)$': 'babel-jest',
   },
 };
